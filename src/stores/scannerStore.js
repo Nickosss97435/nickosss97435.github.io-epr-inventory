f3b3363bd19626a44b6beef3ec10aef391c7fb58
unknown <Administrateur@STUDIO.LOCAL>
2025-01-09 19:09:55 +0400

    mise en ligne

--- src/stores/scannerStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand'
-
-const useScannerStore = create((set) => ({
-  scannerInfo: null,
-  setScannerInfo: (info) => set({ scannerInfo: info }),
-  clearScannerInfo: () => set({ scannerInfo: null }),
-}))
-
-export default useScannerStore
